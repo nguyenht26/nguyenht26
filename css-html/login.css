93ca91c2c432da4832f764614c86e5b239cfabf4
nguyenht26 <100017702+nguyenht26@users.noreply.github.com>
2022-10-29 08:06:13 +0700

    Add files via upload

---
+++ css-html/login.css
@@ -0,0 +1,85 @@
+*{
+    padding: 0px;
+    margin: 0px;
+    font-family: sans-serif;
+    box-sizing: border-box;
+}
+.body{
+    width: 1000px;
+    height: 500px;
+    padding: 20px;
+    border: 2px solid;
+
+}
+.right{
+    width: 50%;
+    float:right;
+    height: 100%;
+    
+}
+.left{
+    width: 50%;
+    height: 100%;
+    float:left;
+    
+}
+.clear{
+    clear: both;
+}
+main{
+    background-color: #dddddd;
+    min-height: 100%;
+    padding: 7.5px 15px;
+}
+.container{
+    width: 100%;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.login-form{
+    width: 100%;
+    max-width: 400px;
+    margin: 20px auto;
+    background-color: #ffffff;
+    padding: 15px;
+    border: 2px dotted #cccccc;
+    border-radius: 10px;
+}
+h1{
+    color: #009999;
+    font-size: 20px;
+    margin-bottom: 30px;
+}
+.input-box{
+    margin-bottom: 10px;
+}
+.input-box input{
+    padding: 7.5px 15px;
+    width: 100%;
+    border: 1px solid #cccccc;
+    outline: none;
+}
+.btn-box{
+    text-align: left;
+    margin-top: 30px;
+}
+.btn-box button{
+    padding: 7.5px 15px;
+    border-radius: 2px;
+    background-color: #009999;
+    color: #ffffff;
+    border: none;
+    outline: none;
+}
+.img{
+    width: 100%;
+    height: 100%;
+}
+.center{
+    text-align: center;
+    margin-top: 30px;
+}
+.space{
+    padding-left: 120px;
+}
